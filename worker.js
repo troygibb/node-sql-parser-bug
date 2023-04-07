2831633cdc258d8a8a8ff4a840b073365387e01a
Troy Gibb <troy.gibb@getcruise.com>
2023-04-07 16:43:54 -0700

    Update

--- worker.js
+++ worker.js
@@ -1,7 +1,12 @@
-importScripts("https://unpkg.com/node-sql-parser/umd/mysql.umd.js")
 
-const parser = new NodeSQLParser.Parser()
-const ast = parser.astify("select id, name from students where age < 18")
-console.log(ast)
-const sql = parser.sqlify(ast)
-console.log(sql)
+try {
+  importScripts("https://unpkg.com/node-sql-parser/umd/mysql.umd.js")
+  const parser = new NodeSQLParser.Parser()
+  const ast = parser.astify("select id, name from students where age < 18")
+  console.log(ast)
+  const sql = parser.sqlify(ast)
+  console.log(sql)
+} catch (e) {
+  console.error(e);
+}
+
